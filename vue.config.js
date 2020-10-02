b4b5922cc6d89b4bd9baf6e63265d79e9c8692b0
ZhangZisu <i@zzs1.cn>
2020-10-02 21:30:23 +0800

    Lint

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,18 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { DefinePlugin } = require("webpack");
-const gitRevision = require("git-revision");
+const { DefinePlugin } = require('webpack')
+const gitRevision = require('git-revision')
 
 module.exports = {
   devServer: {
-    proxy: "http://localhost:3000"
+    proxy: 'http://localhost:3000'
   },
   configureWebpack: {
     plugins: [
       new DefinePlugin({
-        GIT_HASH: JSON.stringify(gitRevision("hash")),
-        GIT_BRANCH: JSON.stringify(gitRevision("branch")),
+        GIT_HASH: JSON.stringify(gitRevision('hash')),
+        GIT_BRANCH: JSON.stringify(gitRevision('branch')),
         BUILD_DATE: JSON.stringify(new Date().toLocaleString())
       })
     ]
   }
-};
+}
